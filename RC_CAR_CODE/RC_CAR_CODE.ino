26a83c145224385c928afab2ac0abefd7b8ebcfb
Dhruva Shaw <dhruvashaw@gmail.com>
2022-04-20 07:55:33 +0530

    finnaly fixed the errors

--- RC_CAR_CODE/RC_CAR_CODE.ino
+++ RC_CAR_CODE/RC_CAR_CODE.ino
@@ -1,32 +1,62 @@
 int state = 0; //bluetooth state
 
-int motorleftpin1 = 1; //left motor pin 1
-int motorleftpin2 = 2; //left motor pin 2
+int motorleftpin2 = 11; //left motor pin 2 (counetr clockwise)
+int motorleftpin1 = 10; //left motor pin 1 (clockwise)
 
-int motorrightpin1 = 3; //right motor pin 1
-int motorrightpin2 = 4; //right motor pin 2
+int motorrightpin2 = 13; //right motor pin 2 (counterclockwise)
+int motorrightpin1 = 12; //right motor pin 1 (clockwise)
 
 void setup()
 {
+  pinMode(motorleftpin1, OUTPUT);
+  pinMode(motorleftpin2, OUTPUT);
+  pinMode(motorrightpin1, OUTPUT);
+  pinMode(motorrightpin2, OUTPUT);
   Serial.begin(9600);
-}
-
-void right(){
-}
-
-void left(){
-}
-
-void forward(){
-}
-
-void backward(){
+  digitalWrite(motorleftpin1, LOW);
+  digitalWrite(motorrightpin1, LOW);
+  digitalWrite(motorrightpin2, LOW);
+  digitalWrite(motorleftpin2, LOW);
 }
 
 void loop()
 {
-  if(Serial.available() > 0){
+  if(Serial.available()){
     state = Serial.read(); // Read the data from the serial port
+    Serial.println(state);
   }
- 
+
+  //Deciding functions for motor
+  if (state == 'F') {
+    Serial.println("Moving Forward");
+    digitalWrite(motorleftpin1, HIGH);
+    digitalWrite(motorrightpin1, HIGH);
+    state = 0;
+
+  }
+  else if (state == 'B') {
+    Serial.println("Moving Backwards");
+    digitalWrite(motorrightpin2, HIGH);
+    digitalWrite(motorleftpin2, HIGH);
+    state = 0;
+
+  }else if (state == 'R') {
+    digitalWrite(motorrightpin1, HIGH);
+    Serial.println("Moving Right");
+    state = 0;
+
+  }else if (state == 'L') {
+    Serial.println("Moving Left");
+    digitalWrite(motorleftpin1, HIGH);
+    state = 0;
+
+  }else if (state == 'S'){
+    Serial.println("Stopping");
+    digitalWrite(motorleftpin1, LOW);
+    digitalWrite(motorrightpin1, LOW);
+    digitalWrite(motorrightpin2, LOW);
+    digitalWrite(motorleftpin2, LOW);
+    state = 0;
+  }
+ delay(100);
 }
